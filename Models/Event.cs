37954ae0e50dc83ff5ddb722c55f2c9133db10ec
StinanH <stina.hedman@chasacademy.se>
2024-10-15 11:53:41 +0200

    event and venue class updated

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EventVault.Models
 {
@@ -7,5 +8,31 @@ namespace EventVault.Models
     {
         [Key]
         public int Id { get; set; }
+
+        string Title;
+
+        String Description;
+
+
+        //if event runs several dates
+
+        List<DateTime> Dates;
+
+        bool requiresTickets;
+
+        bool ticketsAreAvaliable;
+
+        //for pricerange
+
+        Decimal HighestPrice;
+
+        Decimal LowestPrice;
+
+        String EventUrlPage;
+
+        [ForeignKey("Venue")]
+        int FK_Venue;
+
+        Venue Venue;
     }
 }
