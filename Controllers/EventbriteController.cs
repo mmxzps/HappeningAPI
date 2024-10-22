d7b4199e5e24c2eabc121637b11ad7f0b97d0f11
Skokartong <amanda.olving@gmail.com>
2024-10-22 10:14:20 +0200

    Change to private token för Eventbrite so that public information about events is possible without OAuth2.0

--- Controllers/EventbriteController.cs
+++ Controllers/EventbriteController.cs
@@ -1,32 +1,30 @@
-﻿using EventVault.Services.IServices;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
-namespace EventVault.Controllers
+public class EventbriteService
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EventbriteController : ControllerBase
+    private readonly HttpClient _httpClient;
+
+    public EventbriteService(HttpClient httpClient)
     {
-        private readonly IEventbriteServices _eventbriteServices;
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://www.eventbriteapi.com/v3/");
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "YOUR_PRIVATE_TOKEN");
+    }
 
-        public EventbriteController(IEventbriteServices eventbriteServices)
-        {
-            _eventbriteServices = eventbriteServices;
-        }
+    public async Task<string> GetAllEventsAsync()
+    {
+        var response = await _httpClient.GetAsync("events/search/");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
 
-        [HttpGet("events")]
-        public async Task<IActionResult> GetEvents()
-        {
-            var events = await _eventbriteServices.GetEventsAsync();
-            return Ok(events);
-        }
-
-        [HttpGet("events/{id}")]
-        public async Task<IActionResult> GetEventById(string id)
-        {
-            var eventDetails = await _eventbriteServices.GetEventByIdAsync(id);
-            return Ok(eventDetails);
-        }
+    public async Task<string> GetEventByIdAsync(string eventId)
+    {
+        var response = await _httpClient.GetAsync($"events/{eventId}/");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
     }
 }
+
