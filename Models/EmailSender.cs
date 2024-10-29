6de0a3d9bbab94067110b4602db4e643e7426020
Skokartong <amanda.olving@gmail.com>
2024-10-29 17:32:10 +0100

    Add config for yahoo

--- Models/EmailSender.cs
+++ Models/EmailSender.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 using EventVault.Models;
-using MailKit.Net.Smtp;
 using MailKit.Security;
 
 public class EmailSender : IEmailSender
@@ -19,20 +18,19 @@ public class EmailSender : IEmailSender
     public async Task SendEmailAsync(string email, string subject, string message)
     {
         var emailMessage = new MimeMessage();
-        emailMessage.From.Add(new MailboxAddress("EventVault", "eventvault123@gmail.com"));
+        emailMessage.From.Add(new MailboxAddress("EventVault", _smtpSettings.User));
         emailMessage.To.Add(new MailboxAddress("", email));
         emailMessage.Subject = subject;
         emailMessage.Body = new TextPart("html") { Text = message };
 
         using (var client = new SmtpClient())
         {
-            await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-
-            await client.AuthenticateAsync("eventvault123@gmail.com", "EventVault123!?");
-
+            await client.ConnectAsync(_smtpSettings.Server, _smtpSettings.Port, SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_smtpSettings.User, _smtpSettings.Password);
             await client.SendAsync(emailMessage);
             await client.DisconnectAsync(true);
         }
     }
 }
+
 
