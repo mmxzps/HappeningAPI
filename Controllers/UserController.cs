8c62f3520ed5ab53d50a264f406f3109be6c83aa
Stina Hedman <stinan.h@gmail.com>
2024-11-11 12:20:12 +0100

    controller base wip.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using EventVault.Services;
+﻿using EventVault.Models.DTOs;
+using EventVault.Services;
 using EventVault.Services.IServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,72 @@ namespace EventVault.Controllers
             {
                 var allUsers = await _userService.GetAllUsersAsync();
                 return Ok(allUsers);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost("events")]
+        public async Task<IActionResult> AddEventToUser(EventCreateDTO eventCreateDTO, int userId)
+        {
+            try
+            {
+                //add specific event to user
+                //await _userService.AddEventToUser(eventCreateDTO, userId);
+
+                return Ok(eventCreateDTO);
+            }
+
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        public async Task<IActionResult> GetEvent(int userId, int eventId)
+        {
+            try
+            {
+                //get event of user
+                //var event = await _userService.GetUsersEventAsync(userId, eventId);
+                return Ok();
+            }
+
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("events")]
+        public async Task<IActionResult> GetAllEvents(int userID)
+        {
+            try
+            {
+                //get list of events
+                //var events = await _userService.GetAllUserEventsAsync(userId);
+
+                return Ok();
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> DeleteEventFromUser(int userId, int eventId)
+        {
+            try
+            {
+                //remove event from user
+                //await _userService.RemoveEventFromUserAsync(userId, eventId);
+                return Ok();
             }
             catch (Exception ex)
             {
